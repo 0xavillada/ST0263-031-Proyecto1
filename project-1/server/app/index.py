5b1f09f348ae18bb144c8c15b219104ed93a0e63
Andres Villada <avillada@eafit.edu.co>
2019-08-17 00:48:30 -0500

    fallo al subir ficheros

--- project-1/server/app/index.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
-from flask import Blueprint
-import sys
-
-index_app = Blueprint("index_app", __name__, template_folder="templates")
